4ed543d73e2cc60a7dd2308307f34dcc10a0d08a
Lohith N V <n.vlohithnv@gmail.com>
2019-08-31 23:31:58 +0530

    Added toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule, MatIconModule, MatListModule, MatButtonModule } from '@angular/material';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule, MatIconModule, MatListModule, MatButtonModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
